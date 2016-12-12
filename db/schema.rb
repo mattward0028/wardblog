f14c6c5a267bddc0f5dbe93ee4db47c250d70701
mattward0028 <mattward0028@gmail.com>
2016-12-12 14:29:32 -0800

    added background image

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161107200023) do
 
-  create_table "comments", force: :cascade do |t|
-    t.integer  "post_id"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
